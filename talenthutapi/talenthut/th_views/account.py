ecf769bb5a59311297dcd473e9810783fd67dbe0
Nibir Hossain <Nibir.Hossain85@gmail.com>
2018-09-11 22:13:41 +0200

    implemented token based authentication

--- talenthutapi/talenthut/th_views/account.py
+++ talenthutapi/talenthut/th_views/account.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
 from django.core.exceptions import ObjectDoesNotExist
 from ..models import Talent, Recruiter
 from ..th_serializers.talent import TalentSerializer
@@ -10,11 +12,15 @@ from ..th_serializers.recruiter import RecruiterSerializer
 
 class HomeView(APIView):
 
+    permission_classes = (AllowAny, )
+
     def get(self, request):
         return Response('Home Page of TalentHut REST APIs. Version 1.0.0')
 
 
 class LoginView(APIView):
+
+    permission_classes = (AllowAny, )
 
     def post(self, request):
         username = request.data.get('username')
@@ -26,12 +32,14 @@ class LoginView(APIView):
             try:
                 recruiter = Recruiter.objects.get(user=user)
                 serializer = RecruiterSerializer(recruiter)
-                return Response(serializer.data)
+                token, created = Token.objects.get_or_create(user=user)
+                return Response({'token': token.key, 'recruiter': serializer.data})
             except ObjectDoesNotExist:
                 try:
                     talent = Talent.objects.get(user=user)
                     serializer = TalentSerializer(talent)
-                    return Response(serializer.data)
+                    token, created = Token.objects.get_or_create(user=user)
+                    return Response({'token': token.key, 'talent': serializer.data})
                 except ObjectDoesNotExist:
                     return Response({"Exception": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
         else:
--- talenthutapi/talenthutapi/conf/base.py
+++ talenthutapi/talenthutapi/conf/base.py
@@ -58,9 +58,13 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ]
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 MIDDLEWARE = [
